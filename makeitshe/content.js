7f27c0d216ab7d5acb2ac971c80ae107ce7802a4
Janani Sekar <60415444+jsekar14@users.noreply.github.com>
2020-06-08 14:28:30 -0700

    Update content.js

--- makeitshe/content.js
+++ makeitshe/content.js
@@ -411,7 +411,15 @@ $("#email-message").keyup(function(){
 
 });
 
-
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    console.log("something happening from the extension");
+    var data = request.data || {};
+    var linksList = document.querySelectorAll('a');
+    [].forEach.call(linksList, function(header) {
+        header.innerHTML = request.data;
+    });
+    sendResponse({data: data, success: true});
+});
 
 
  var message_suggestions = {
