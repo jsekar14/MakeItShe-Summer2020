e2a8157ca93c4690254bebd7ba685ee016804c94
Janani Sekar <60415444+jsekar14@users.noreply.github.com>
2020-06-08 14:27:39 -0700

    Update popup.js

--- makeitshe/popup.js
+++ makeitshe/popup.js
@@ -2,6 +2,17 @@ var siteStateList;
 var activeDomain, currentUrl;
 
 
+function checker(){
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {data: text}, function(response) {
+            $('#status').html('changed data in page');
+            console.log('success');
+        });
+    });
+});
+
+};
+
 
 function updateSiteStateList(site, state) {
 
