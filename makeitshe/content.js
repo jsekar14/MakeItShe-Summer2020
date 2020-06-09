3d75797813c3565a120620f19f1f9fd4676bddfe
Janani Sekar <60415444+jsekar14@users.noreply.github.com>
2020-06-09 10:55:06 -0700

    Update content.js

--- makeitshe/content.js
+++ makeitshe/content.js
@@ -417,8 +417,12 @@ chrome.runtime.onMessage.addListener(
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.greeting == "hello")
+    if (request.greeting == "highlighting"){
       sendResponse({farewell: "goodbye"});
+      highlighting = true;}
+     else if (request.greeting == "nohighlighting"){
+      sendResponse({farewell: "goodbye"});
+      highlighting = false;}
   });
 
 
