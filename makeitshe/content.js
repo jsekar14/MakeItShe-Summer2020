4ffe7800cae0043ef446a3f6d1fd415705f8c558
Janani Sekar <60415444+jsekar14@users.noreply.github.com>
2020-07-01 08:47:53 -0700

    Update content.js

--- makeitshe/content.js
+++ makeitshe/content.js
@@ -431,18 +431,6 @@ chrome.runtime.onMessage.addListener(
     }
   });
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "reset")
-    {
-      highlighting = false;
-      applyContent();
-      sendResponse({farewell: "goodbye"});
-    }
-  });
 
 
 
